c0ec99dea9f7e7e1f7d9df97ec0f8e42f8855d9e
Annacarvalhotec <112017901+Annacarvalhotec@users.noreply.github.com>
2023-02-14 16:58:06 -0300

    Add files via upload

---
+++ joalheria/index.php
@@ -0,0 +1,155 @@
+<?php
+    require_once 'head.php';
+    require_once 'menu.php';
+
+
+?>
+
+    <section class="first-section" id="home">
+        
+        <div class="conteudo-principal">
+            <h1></h1>
+            <h2>Deixe-nos realizar Seu Sonho,Temos As Melhores Joias</h2>
+            <div class="btn">
+                <button class="reservar">Reserve a sua!</button>
+                <button class="montar">Monte seu pedido!</button>
+            </div>
+        </div>    
+    </section>    
+
+    <section class="sobre-nos" id="sobrenos"> 
+        <div class="main">
+            <div class="contentsobre">
+                <h2>Sobre nós</h2>
+                <p>Nossa joalheria é unica somos epecialistas em realizar sonhos,nossa empresa nasceu do sonho de 3 amigas que são apaixonada por joias e beleza,nossos produtos são confeccionados com tecnologia e obra prima,tudo com um toque único nossas peças são feitas sob encomenda,deixe-nos realizar seus sonhos. 
+                </p>
+            </div>     
+            <div class="img-joias">
+                <img src="./img/PEROLA LUXO.jpg" alt="PÉROLA">
+            </div>
+        </div>    
+    </section>
+
+    <section class="joias" id="joias">
+        <h2>Nossas Joias</h2>
+
+            <div class="container-fluid itens-">
+                <div class="row">
+                <div class="pulseira bebê">
+                    <img src="./img/pulseira de bebe morangos.jfif" alt="pulseira">
+                    <div class="info">
+                        <h3>Colar Infantil</h3>
+                        <h4>prata <span>R$300,00</span></h4>
+                        <h4>ouro <span>R$400,00</span></h4>
+                        <h4>ouro branco <span>R$500,00</span></h4>
+                        <button class="pedir">pedir agora</button>
+                    </div>   
+                </div>
+                <div class="pulseira bebê">
+                    <img src="./img/pulseira de bebe.jfif" alt="pulseira">
+                    <div class="info">
+                        <h3>Pulseira Infantil</h3>
+                        <h4>prata <span>R$300,00</span></h4>
+                        <h4>ouro <span>R$400,00</span></h4>
+                        <h4>ouro branco <span>R$500,00</span></h4>
+                        <button class="pedir">pedir agora</button>
+                    </div> 
+                
+                </div>
+                <div class="pulseira">
+                    <img src="./img/unnamed.jpg" alt="colar">
+                    <div class="info">
+                        <h3>Pulseiras para Adultos</h3>
+                        <h4>prata <span>R$300,00</span></h4>
+                        <h4>ouro <span>R$400,00</span></h4>
+                        <h4>ouro branco <span>R$500,00</span></h4>
+                        <button class="pedir">pedir agora</button>
+                    </div> 
+                
+                </div>
+                <div class="anéis">
+                    <img src="./img/unnamed.jpg" alt="anél">
+                    <div class="info">
+                        <h3>Anéis</h3>
+                        <h4>prata <span>R$300,00</span></h4>
+                        <h4>ouro <span>R$400,00</span></h4>
+                        <h4>ouro branco <span>R$500,00</span></h4>
+                        <button class="pedir">pedir agora</button>
+                    </div> 
+                
+                </div>
+                <div class="anéis">
+                    <img src="./img/unnamed.jpg" alt="anél">
+                    <div class="info">
+                        <h3>Anéis</h3>
+                        <h4>prata <span>R$300,00</span></h4>
+                        <h4>ouro <span>R$400,00</span></h4>
+                        <h4>ouro branco <span>R$500,00</span></h4>
+                        <button class="pedir">pedir agora</button>
+                    </div> 
+                
+                </div>
+                <div class="brincos">
+                    <img src="./img/unnamed.jpg" alt="brincos">
+                    <div class="info">
+                        <h3>Brincos</h3>
+                        <h4>prata<span>R$300,00</span></h4>
+                        <h4>ouro <span>R$400,00</span></h4>
+                        <h4>ouro branco <span>R$500,00</span></h4>
+                        <button class="pedir">pedir agora</button>
+                    </div> 
+                
+                </div>
+                <div class="pulseira masculina">
+                    <img src="./img/unnamed.jpg" alt="pulseira">
+                    <div class="info">
+                        <h3>Pulseira Masculina</h3>
+                        <h4>prata <span>R$350,00</span></h4>
+                        <h4>ouro <span>R$450,00</span></h4>
+                        <h4>ouro branco <span>R$550,00</span></h4>
+                        <button class="pedir">pedir agora</button>
+                    </div> 
+                
+                </div>
+                <div class="colar masculino">
+                    <img src="./img/unnamed.jpg" alt="Colar">
+                    <div class="info">
+                        <h3>Colar Masculino</h3>
+                        <h4>prata <span>R$400,00</span></h4>
+                        <h4>ouro <span>R$450,00</span></h4>
+                        <h4>ouro branco <span>R$550,00</span></h4>
+                        <button class="pedir">pedir agora</button>
+                    </div> 
+                </div>
+                    <div class="anéis masculinos">
+                        <img src="./img/unnamed.jpg" alt="anéis">
+                        <div class="info">
+                            <h3>Anéis</h3>
+                            <h4>prata <span>R$350,00</span></h4>
+                            <h4>ouro <span>R$450,00</span></h4>
+                            <h4>ouro branco <span>R$550,00</span></h4>
+                            <button class="pedir">pedir agora</button>
+                        </div> 
+                    </div>
+                    <div class="brincos">
+                        <img src="./img/unnamed.jpg" alt="brincos">
+                        <div class="info">
+                            <h3>brincos</h3>
+                            <h4>prata <span>R$300,00</span></h4>
+                            <h4>ouro <span>R$400,00</span></h4>
+                            <h4>ouro branco <span>R$500,00</span></h4>
+                            <button class="pedir">pedir agora</button>
+                        </div>
+                    </div>
+                   
+                </div>
+                
+            </div>
+
+    </section>
+
+   <?php
+
+   require_once 'footer.php';
+
+   ?>
---
+++ joalheria/menu.php
@@ -0,0 +1,26 @@
+<header>
+            <div class="content">
+                <div class="logo">
+                    <img src="./img/PEROLA LUXO.jpg" alt="PÉROLA">
+                    <h3>VILLA JOALHERIA</h3>
+                </div>
+                <ul class="list-menu">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#sobrenos">Sobre nós</a></li>
+                    <li><a href="#joias">JOIAS</a></li>
+                    <li><a href="#contatos">Contatos</a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+
+                </ul>
+
+                <div class="menu-toggle">
+                    <div class="one"></div>
+                    <div class="two"></div>
+                    <div class="three"></div>
+                </div>
+
+            </div>
+        
+    </header>
